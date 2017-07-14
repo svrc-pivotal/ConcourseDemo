017a7c0dbb08263e9f32b5659b0c1d3a1f6349ca
Stuart Charlton <scharlton@pivotal.io>
2017-07-14 09:58:29 -0400

    First Commit

---
+++ src/main/java/com/rbc/ConcourseDemo/ConcourseDemoApplication.java
@@ -0,0 +1,12 @@
+package com.rbc.ConcourseDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcourseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcourseDemoApplication.class, args);
+	}
+}
